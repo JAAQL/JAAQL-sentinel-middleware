d5847a3f7acee1495aab7eb4c48566d7eb77217f
Aaron Tasker <45070792+aarontasker@users.noreply.github.com>
2022-08-17 19:13:08 +0200

    Version 1.0.16

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+ECHO This script will build jaaql sentinel and push to pypi and docker
+set /p VERSION="Please input version: "
+docker build -t jaaql/jaaql-sentinel-middleware -f docker/Dockerfile .
+docker tag jaaql/jaaql-sentinel-middleware jaaql/jaaql-sentinel-middleware:%VERSION%
+docker push jaaql/jaaql-sentinel-middleware
+docker push jaaql/jaaql-sentinel-middleware:%VERSION%
